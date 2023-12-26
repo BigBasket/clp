f9f71537b54281667bd7015d571742c28cd405c9
Raja Karthik <vobugari.rajakarthik@bigbasket.com>
2023-12-26 13:46:06 +0530

    BH-4879 Added package to install

--- components/core/tools/scripts/lib_install/ubuntu-focal/install-prebuilt-packages.sh
+++ components/core/tools/scripts/lib_install/ubuntu-focal/install-prebuilt-packages.sh
@@ -8,11 +8,12 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
   curl \
   build-essential \
   git \
-  g++-10 \
+  g++-10.3.0 \
   gcc-10 \
   libboost-filesystem-dev \
   libboost-iostreams-dev \
   libboost-program-options-dev \
+  libstdc++-10-dev \
   libmariadb-dev \
   libssl-dev \
   openjdk-11-jdk \
