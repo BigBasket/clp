a6ffc6ec42c7edbdcba49ab34b692aceb913798a
Raja Karthik <vobugari.rajakarthik@bigbasket.com>
2023-12-28 18:06:23 +0530

    BH-4879 Added logs

--- components/core/src/clo/clo.cpp
+++ components/core/src/clo/clo.cpp
@@ -177,7 +177,7 @@ static SearchFilesResult search_files(
         std::atomic_bool const& query_cancelled,
         int controller_socket_fd
 ) {
-    SPDLOG_TRACE("search_files start")
+    SPDLOG_TRACE("search_files start");
     SearchFilesResult result = SearchFilesResult::Success;
 
     File compressed_file;
@@ -331,7 +331,7 @@ int main(int argc, char const* argv[]) {
     // Program-wide initialization
     try {
         // auto stderr_logger = spdlog::stderr_logger_st("stderr");
-        spdlog::set_level(SPDLOG_LEVEL_TRACE)
+        spdlog::set_level(0);
         // spdlog::set_default_logger(stderr_logger);
         spdlog::set_pattern("%Y-%m-%d %H:%M:%S,%e [%l] %v");
     } catch (std::exception& e) {
