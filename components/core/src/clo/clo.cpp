742cbc174679515658c978177abb7e0785cef235
Raja Karthik <vobugari.rajakarthik@bigbasket.com>
2023-12-28 08:56:11 +0530

    BH-4879 Fixed typo

--- components/core/src/clo/clo.cpp
+++ components/core/src/clo/clo.cpp
@@ -152,7 +152,8 @@ static ErrorCode send_result(
         int controller_socket_fd
 ) {
     boost::property_tree::ptree pt;
-    std::stringstream ss(decompressed_msg);
+    printf("%s", decompressed_msg.c_str())
+    std::stringstream ss(decompressed_msg.c_str());
     boost::property_tree::read_json(ss, pt);
     int64_t datetime = compressed_msg.get_ts_in_milli()/1000;
     pt.put("log_time", ctime(&datetime));
